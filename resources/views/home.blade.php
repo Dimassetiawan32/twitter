51f79d5809d7993f3e48e000c1b9b23cf1bda7a3
Dimassetiawan32 <dmas0097@gmail.com>
2020-02-20 17:24:42 +0700

    Second

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,39 +10,32 @@
             <div class="card border-0">
                 <div class="d-flex card-header border-0">
                     <div class="mr-auto">
-                        <h6 class="text-muted">Tweet</h6>
+                        <h6 class="text-muted">
+                            Tweet
+                        </h6>
                     </div>
                     <div>
                         <svg fill="none" height="24" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="2"/><circle cx="12" cy="4" r="2"/><circle cx="12" cy="20" r="2"/></svg>    
                     </div>
                 </div>
                     <div class="card-body">
-                        <div>
-                            <div class="d-flex">
-                                <img src="{{asset('images/Potoaja.jpg')}}" width="50" height="50" class="rounded-circle" alt="">
-                            </div>
-                            <div class="col-md-9">
-                                <div class="d-flex">
-                                    <div class="mr-auto"></div>
-                                </div>
-                            </div>
+                        <div class="d-flex">
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="50" height="50" class="rounded-circle" alt="">   
+                        <div class="ml-3">
                             <h6 class="text-dark">
                                 Dimas
-                                <h6 class="text-muted">@diims</h6>
+                                <small class="text-muted">@diims</small>
                             </h6>
-                            <p class="text-primary">@dev,
-                                <p class="text-muted">
-                                    Lorem ipsum dolor sit amet,
-                                    consectetur adipiscing elit,
-                                    sed do.
-                                </p>
-                            </p>
+                            <h6 class="text-primary">
+                                @dev, 
+                                <span class="text-muted">
+                                    Lorem ipsum dolor sit amet, Consectetur adipiscing elit, Sed do.
+                                </span>
+                            </h6>
                         </div>
-                        <div>
-                            
+                        
                         </div>
-                        </div>
-
+                        
                     </div>
                 </div>
             </div>
