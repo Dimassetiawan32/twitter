a197a77b8742707bcd096af26cf2364266a1d07b
Dimassetiawan32 <dmas0097@gmail.com>
2020-02-24 13:43:45 +0700

    proggres

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,71 @@
 <div class="container">
     <div class="row">
         <div class="col-md-3">
-
+            <div class="row justify-content-center">
+                <div class="container">
+                    <div class="card border-top-0" style="margin-top: -25px;">
+                        <div class="card-body">
+                            <div class="mr-auto">
+                                <span>Who To Follow</span>
+                            </div>
+                            <div class="text-right" style="margin-top: -22px;">
+                                <span class="text-primary">
+                                    Refresh See All
+                                </span>
+                            </div>
+                            <br>
+                            <svg enable-background="new 0 0 512 512" height="40px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="342.4" x2="342.4" y1="213.4" y2="267.9"/><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="369.6" x2="315.2" y1="240.6" y2="240.6"/><circle cx="256" cy="213.4" fill="none" r="34.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10"/><path d="   M318.4,331.7c0-34.5-27.9-62.4-62.4-62.4s-62.4,27.9-62.4,62.4H318.4z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" /></g></svg>
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="20" height="20" class="rounded-circle" alt="">
+                                <span class="text-dark" style=”text-align:justify;”>
+                                    Dimas
+                                <small class="text-muted">@diims</small>
+                                </span>
+                                <div class="ml-5">
+                                <span class="text-muted">Lorem Ipsum has been the industry's</span>
+                                </div>
+                            <svg enable-background="new 0 0 512 512" height="40px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="342.4" x2="342.4" y1="213.4" y2="267.9"/><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="369.6" x2="315.2" y1="240.6" y2="240.6"/><circle cx="256" cy="213.4" fill="none" r="34.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10"/><path d="   M318.4,331.7c0-34.5-27.9-62.4-62.4-62.4s-62.4,27.9-62.4,62.4H318.4z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" /></g></svg>
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="20" height="20" class="rounded-circle" alt="">
+                                <span class="text-dark" style=”text-align:justify;”>
+                                    Dimas
+                                <small class="text-muted">@diims</small>
+                                </span>
+                                <div class="ml-5">
+                                <span class="text-muted">Lorem Ipsum has been the industry's</span>
+                                </div>
+                            <svg enable-background="new 0 0 512 512" height="40px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="342.4" x2="342.4" y1="213.4" y2="267.9"/><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="369.6" x2="315.2" y1="240.6" y2="240.6"/><circle cx="256" cy="213.4" fill="none" r="34.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10"/><path d="   M318.4,331.7c0-34.5-27.9-62.4-62.4-62.4s-62.4,27.9-62.4,62.4H318.4z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" /></g></svg>
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="20" height="20" class="rounded-circle" alt="">
+                                <span class="text-dark" style=”text-align:justify;”>
+                                    Dimas
+                                <small class="text-muted">@diims</small>
+                                </span>
+                                <div class="ml-5">
+                                <span class="text-muted">Lorem Ipsum has been the industry's</span>
+                                </div>                            
+                            <svg enable-background="new 0 0 512 512" height="40px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="342.4" x2="342.4" y1="213.4" y2="267.9"/><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="369.6" x2="315.2" y1="240.6" y2="240.6"/><circle cx="256" cy="213.4" fill="none" r="34.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10"/><path d="   M318.4,331.7c0-34.5-27.9-62.4-62.4-62.4s-62.4,27.9-62.4,62.4H318.4z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" /></g></svg>
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="20" height="20" class="rounded-circle" alt="">
+                                <span class="text-dark" style=”text-align:justify;”>
+                                    Dimas
+                                <small class="text-muted">@diims</small>
+                                </span>
+                                <div class="ml-5">
+                                <span class="text-muted">Lorem Ipsum has been the industry's</span>
+                                </div>                            
+                            <br>
+                            <span class="text-primary">Popular Accounts Find Friends</span>
+                            <br>
+                            <br>
+                            <span>Trends <span class="text-primary">Change</span></span>
+                            <div>
+                            <span class="text-primary">#Windows10pro</span>
+                            <div>
+                            <span class="text-primary">#Iphone11</span>
+                            </div>
+                            <span class="text-primary">#macbookPro</span>
+                            </div>
+                        </div>    
+                    </div>
+                </div> 
+            </div>
         </div>
         <div class="col-md-6">
             <div class="card border-0">
@@ -15,34 +79,130 @@
                         </h6>
                     </div>
                     <div>
-                        <svg fill="none" height="24" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="2"/><circle cx="12" cy="4" r="2"/><circle cx="12" cy="20" r="2"/></svg>    
+                        <svg fill="none" height="24" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="2"/><circle cx="12" cy="4" r="2"/><circle cx="12" cy="20" r="2"/></svg>
                     </div>
                 </div>
-                    <div class="card-body">
-                        <div class="d-flex">
-                            <img src="{{asset('images/Potoaja.jpg')}}" width="50" height="50" class="rounded-circle" alt="">   
+            <div class="card-body">
+                <div class="d-flex">
+                     <img src="{{asset('images/Potoaja.jpg')}}" width="50" height="50" class="rounded-circle" alt="">   
                         <div class="ml-3">
                             <h6 class="text-dark">
                                 Dimas
-                                <small class="text-muted">@diims</small>
+                                <small class="text-muted">
+                                    @diims
+                                </small>
                             </h6>
                             <h6 class="text-primary">
-                                @dev, 
+                                @dev.
                                 <span class="text-muted">
                                     Lorem ipsum dolor sit amet, Consectetur adipiscing elit, Sed do.
                                 </span>
                             </h6>
                         </div>
-                        
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="d-flex">
+                    <img src="{{asset('images/Potoaja.jpg')}}" width="50" height="50" class="rounded-circle" alt="">
+                        <div class="ml-3">
+                            <h6 class="text-dark">
+                                Dimas
+                                <small class="text-muted"> 
+                                    @diims
+                                </small>
+                                </h6>
+                                <span class="text-muted"> 
+                                    Lorem ipsum dolor sit amet, Consectetur adipiscing elit, Sed do.
+                                </span>
+                                <img src="{{asset('images/Potoaja.jpg')}}" width="100px" height="100px" alt="">
                         </div>
-                        
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex">
+                        <img src="{{asset('images/Potoaja.jpg')}}" width="50" height="50" class="rounded-circle" alt="">
+                            <div class="ml-3">
+                                <h6 class="text-dark">
+                                     Dimas
+                                <small class="text-muted">@diims</small>
+                                </h6>
+                                <h6 class="text-primary">
+                                    @dev,
+                                    <span class="text-muted">
+                                        Lorem ipsum dolor sit amet, Consectetur adipiscing elit, Sed do.
+                                    </span>
+                                </h6>
+                            </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-3">
-        
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="card border-0 shadow">
+                        <div class="card-body">
+                            <h4 class="font-weight-bold">
+                                You're tweet activity
+                            </h4>
+                            <span class="text-muted"> lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do. <span class="text-primary">read more</span></span>
+                            <div class="py-5">
+                                <button class="btn btn-info btn-block">
+                                    <h3><img src="{{('images/logo.png')}}" alt="" class="" height="30">Tweet</h3>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>        
+            </div> 
         </div>
     </div>
 </div>
 @endsection
+
+<!-- <svg enable-background="new 0 0 512 512" height="40px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="342.4" x2="342.4" y1="213.4" y2="267.9"/><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="369.6" x2="315.2" y1="240.6" y2="240.6"/><circle cx="256" cy="213.4" fill="none" r="34.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10"/><path d="   M318.4,331.7c0-34.5-27.9-62.4-62.4-62.4s-62.4,27.9-62.4,62.4H318.4z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" /></g></svg>
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="20" height="20" class="rounded-circle" alt="">
+                                <span class="text-dark" style=”text-align:justify;”>
+                                    Dimas
+                                <small class="text-muted">@diims</small>
+                                <p class="text-muted">Lorem Ipsum has been the industry's</p>
+                                <div class="ml-5">
+                                </span>
+                                </div>
+                        <svg enable-background="new 0 0 512 512" height="40px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="342.4" x2="342.4" y1="213.4" y2="267.9"/><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="369.6" x2="315.2" y1="240.6" y2="240.6"/><circle cx="256" cy="213.4" fill="none" r="34.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10"/><path d="   M318.4,331.7c0-34.5-27.9-62.4-62.4-62.4s-62.4,27.9-62.4,62.4H318.4z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" /></g></svg>
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="20" height="20" class="rounded-circle" alt="">
+                                <span class="text-dark" style=”text-align:justify;”>
+                                    Dimas
+                                <small class="text-muted">@diims</small>
+                                </span>
+                                <div class="ml-5">
+                                <span class="text-primary">
+                                    @dev,
+                                <span class="text-muted">Lorem Ipsum has been the industry's</span>
+                                </span>
+                                </div>
+                        <svg enable-background="new 0 0 512 512" height="40px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="342.4" x2="342.4" y1="213.4" y2="267.9"/><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="369.6" x2="315.2" y1="240.6" y2="240.6"/><circle cx="256" cy="213.4" fill="none" r="34.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10"/><path d="   M318.4,331.7c0-34.5-27.9-62.4-62.4-62.4s-62.4,27.9-62.4,62.4H318.4z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" /></g></svg>
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="20" height="20" class="rounded-circle" alt="">
+                                <span class="text-dark" style=”text-align:justify;”>
+                                    Dimas
+                                <small class="text-muted">@diims</small>
+                                </span>
+                                <div class="ml-5">
+                                <span class="text-primary">
+                                    @dev,
+                                <span class="text-muted">Lorem Ipsum has been the industry's</span>
+                                </span>
+                                </div>
+                        <svg enable-background="new 0 0 512 512" height="40px" id="Layer_1" version="1.1" viewBox="0 0 512 512" width="40px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="342.4" x2="342.4" y1="213.4" y2="267.9"/><line fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" x1="369.6" x2="315.2" y1="240.6" y2="240.6"/><circle cx="256" cy="213.4" fill="none" r="34.5" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10"/><path d="   M318.4,331.7c0-34.5-27.9-62.4-62.4-62.4s-62.4,27.9-62.4,62.4H318.4z" fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="10" /></g></svg>
+                            <img src="{{asset('images/Potoaja.jpg')}}" width="20" height="20" class="rounded-circle" alt="">
+                                <span class="text-dark" style=”text-align:justify;”>
+                                    Dimas
+                                <small class="text-muted">@diims</small>
+                                </span>
+                                <div class="ml-5">
+                                <span class="text-primary">
+                                    @dev,
+                                <span class="text-muted">Lorem Ipsum has been the industry's</span>
+                                </span>
+                                </div> -->
+                                <br>
